96e3428cbe8fef6cc401f1a633505f6411c8df30
shon <turayevshahzodbek@gmail.com>
2024-01-31 16:00:19 +0900

    feat: config.js file starting script creation

---
+++ train.js
@@ -0,0 +1,14 @@
+/*Primitive methods*/
+
+// pm2 start servers.js --name=PuppyHome
+// pm2 start 'npm run dev' --name=PuppyHome
+// pm2 logs
+// pm2 logs 0
+// pm2 kill
+// pm2 stop 1,
+// pm2 delete 0,
+// pm2 restart 1
+
+/*config.js file*/
+// pm2 start process.config.js --env development
+// pm2 start process.config.js --env production
